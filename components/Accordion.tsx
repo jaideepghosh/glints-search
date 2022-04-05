a12712c15a65dd99966d896d1de2b17ac95744c0
jaideepghosh <er.jaideepghosh@gmail.com>
2022-04-06 00:49:59 +0530

    feat:Commented Bookmark toggle template.

--- components/Accordion.tsx
+++ components/Accordion.tsx
@@ -28,6 +28,9 @@ export default function Accordion({ title, children }: AccordionProps) {
     return (
         <div className="border border-gray-400 mb-2">
           <div className={"bg-gray-200 p-4 flex justify-between"}  onClick={HandleOpening}>
+            {/* <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                <path strokeLinecap="round" strokeLinejoin="round" d="M5 5a2 2 0 012-2h10a2 2 0 012 2v16l-7-3.5L5 21V5z" />
+            </svg> */}
             <h4 className="font-semibold">{title}</h4>
             {isOpened ? <ChevronOpen /> : <ChevronClosed />}
           </div>
