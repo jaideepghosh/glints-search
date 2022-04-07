b43a9fb1f7cea6eb0ffe0dbeacf23cd2dffb63cb
jaideepghosh <er.jaideepghosh@gmail.com>
2022-04-07 15:28:49 +0530

    fix:Updated login icon.

--- components/Header.tsx
+++ components/Header.tsx
@@ -32,7 +32,7 @@ export default function Header() {
                             <SignedOut>
                                 <Link href="/login" passHref>
                                     <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
-                                        <path strokeLinecap="round" strokeLinejoin="round" d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1" />
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z" />
                                     </svg>
                                 </Link>
                             </SignedOut>
